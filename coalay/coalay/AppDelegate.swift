00903c2a62d7aa6882353519c31eb956ffed32ca
ochiyu <yuya-ochiiai@outlook.jp>
2020-10-31 23:54:39 +0900

    [add] icon image

---
+++ coalay/coalay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  coalay
+//
+//  Created by 落合裕也 on 2020/10/31.
+//  Copyright © 2020 落合裕也. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ coalay/coalayTests/coalayTests.swift
@@ -0,0 +1,34 @@
+//
+//  coalayTests.swift
+//  coalayTests
+//
+//  Created by 落合裕也 on 2020/10/31.
+//  Copyright © 2020 落合裕也. All rights reserved.
+//
+
+import XCTest
+@testable import coalay
+
+class coalayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
