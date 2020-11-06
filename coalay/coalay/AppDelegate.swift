e5fb445afcdc02a2bea992a5509901620a2ee040
ochiyu <yuya-ochiiai@outlook.jp>
2020-11-07 06:26:57 +0900

    [add] opaque

--- coalay/coalay/AppDelegate.swift
+++ coalay/coalay/AppDelegate.swift
@@ -11,29 +11,26 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     var window: NSWindow!
-
-
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
+        let width = NSScreen.main?.frame.width ?? 400
+        let height = NSScreen.main?.frame.height ?? 400
+        //let contentView = ContentView()
+        let contentView = TransparentWindow(width: width, height: height)
+        window = OpaqueWindow(width: width, height: height)
         window.setFrameAutosaveName("Main Window")
         window.contentView = NSHostingView(rootView: contentView)
         window.makeKeyAndOrderFront(nil)
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
